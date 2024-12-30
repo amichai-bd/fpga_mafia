e5be4a43d8abcff33ad7ed848a9ff909f66bd47e
roman gilgor <roman329@gmail.com>
2024-12-30 10:26:07 +0200

    connect registers to components using tri state grid

--- source/cpuc/cpuc.sv
+++ source/cpuc/cpuc.sv
@@ -14,9 +14,125 @@ import cpuc_package::*;
 (
     input logic clk,
     input logic rst,
-    output var t_reg_outputs reg_output
+    output var t_reg_outputs reg_outputs
 );
 
+// TODO - add multiplication on integers
+// TODO - add FO operations
+// TODO - add types of RAM's
+// TODO - add constants
+// TODO - add mux
+
+//*************************************
+//            GRID DESIGN
+//*************************************
+
+/*  example of the grid for registers. Number of columns are the number of registers
+    and the number of rows are the number of components that can be connected to any register.
+
+      R0 R1 R2 R3
+      __ __ __ __
+ R0  |__|__|__|__|
+ R1  |__|__|__|__|
+ R2  |__|__|__|__|
+ R3  |__|__|__|__|
+  +  |__|__|__|__|
+ ==  |__|__|__|__|
+  >  |__|__|__|__|
+  *  |__|__|__|__|
+
+*/
 
 
+//*************************************
+//        COMPONENTS OUTPUT
+//*************************************
+// TODO - follow the pattern when adding new components
+logic [0:NUM_OF_REGS+NUM_OF_PC-1][DATA_WIDTH-1:0]  regs_array_output;
+logic [0:NUM_OF_ADDERS-1][DATA_WIDTH-1:0]          adders_array_output;
+logic [0:NUM_OF_CMP-1][DATA_WIDTH-1:0]             greaters_array_output;
+logic [0:NUM_OF_EQUAL-1][DATA_WIDTH-1:0]           compares_array_output;
+
+genvar i_regs;
+generate;
+    for(i_regs=0;i_regs<=NUM_OF_REGS+NUM_OF_PC;i_regs++) begin: registers // including pc
+       cpuc_register cpuc_registers
+       (
+         .clk(clk),
+         .rst(rst),
+         .data_in(),
+         .data_out(regs_array_output[i_regs])
+       ); 
+    end 
+endgenerate
+
+
+// cpuc adders
+genvar i_adders;
+generate
+     for(i_adders=0;i_adders<NUM_OF_ADDERS;i_adders++) begin: adders
+       cpuc_adder cpuc_adders
+       (
+         .data_in1(),
+         .data_in2(),
+         .data_out(adders_array_output[i_adders])
+       ); 
+    end  
+endgenerate
+
+// cpuc operator`>`
+genvar i_greater;
+generate
+     for(i_greater=0;i_greater<NUM_OF_CMP;i_greater++) begin: is_greater_operator
+       cpuc_cmp cpuc_greater
+       (
+         .data_in1(),
+         .data_in2(),
+         .data_out(greaters_array_output[i_greater])
+       ); 
+    end  
+endgenerate
+
+// cpuc operator `==`
+genvar i_compare;
+generate
+     for(i_compare=0;i_compare<NUM_OF_EQUAL;i_compare++) begin: comparison_operator
+       cpuc_equal cpuc_compare
+       (
+         .data_in1(),
+         .data_in2(),
+         .data_out(compares_array_output[i_compare])
+       ); 
+    end  
+endgenerate
+
+logic [0:NUM_OF_COMPONENTS-1][DATA_WIDTH-1:0] component_outputs ;
+assign component_outputs = {regs_array_output, adders_array_output, greaters_array_output, compares_array_output};
+
+logic [0:NUM_OF_COMPONENTS-1][0:NUM_OF_REGS+NUM_OF_PC-1][DATA_WIDTH-1:0] register_inputs_2d_grid ; // vertical (column) belongs to specific register
+                                                                                                   // and contains outputs of all the components
+                                                                                                   // all the columns are duplications cause the represent the registers
+                                                                                                   // and all the registers gets the same data
+
+genvar c_tri_state, i_tri_state; // each column 'c' has 'i' tri-states coming from other component outputs
+generate
+      for(c_tri_state=0; c_tri_state < NUM_OF_REGS+NUM_OF_PC; c_tri_state++) begin :inputs_over_all_registers
+          for(i_tri_state=0; i_tri_state < NUM_OF_COMPONENTS; i_tri_state++) begin :index_over_individual_register
+                cpuc_tristate
+                (
+                    .en(),
+                    .data_in(component_outputs[NUM_OF_COMPONENTS]),
+                    .data_out(register_inputs_2d_grid[i_tri_state][c_tri_state]) 
+                );
+
+          end
+      end
+endgenerate
+// outputs of the cpu are the register values
+integer i;
+always_comb begin
+    for (i=0;i<NUM_OF_REGS+NUM_OF_PC;i++) begin
+        reg_outputs[i] = regs_array_output[i];
+    end
+end
 endmodule
--- source/cpuc/cpuc_package.sv
+++ source/cpuc/cpuc_package.sv
@@ -10,29 +10,37 @@
 
 package cpuc_package;
 
-parameter SIGNED_CMP = 1;    // by default allow negative numbers 
+parameter SIGNED_CMP = 1;    // by default allow signed operations 
 parameter DATA_WIDTH = 32;   // width of data in the cpu
 
 parameter MEM_SIZE   = 8096; // mem size of 8096*32 = 32kbyte
-parameter ADDR_WIDTH = $clog2(MEM_SIZE);   // width of adress in the memory
+parameter ADDR_WIDTH = $clog2(MEM_SIZE);   // width of address in the memory
 
 // number of instantiated units
-parameter NUM_OF_REGS     = 32;  // number of registers
-parameter NUM_OF_MUL      = 16;  // number of multipliers
-parameter NUM_OF_ADDERS   = 16;  // number of adders
-parameter NUM_OF_CMP      = 16;  // number of comperators
-parameter NUM_OF_MUX      = 1;   // number of muxes
-parameter NUM_OF_EQUAL    = 16;  // number of is equal
-parameter NUM_OF_SING_MEM = 2;   // number of is single prot ram
-parameter NUM_OF_DUAL_MEM = 2;   // number of is dual port ram
-parameter NUM_OF_QUAD_MEM = 2;   // number of is quad port ram
+parameter NUM_OF_REGS     = 8;  // number of registers
+parameter NUM_OF_MUL      = 0;   // number of multipliers
+parameter NUM_OF_ADDERS   = 2;   // number of adders
+parameter NUM_OF_CMP      = 2;   // number of comperators
+parameter NUM_OF_MUX      = 0;   // number of muxes
+parameter NUM_OF_EQUAL    = 2;   // number of is equal
+parameter NUM_OF_SING_MEM = 0;   // number of is single prot ram
+parameter NUM_OF_DUAL_MEM = 0;   // number of is dual port ram
+parameter NUM_OF_QUAD_MEM = 0;   // number of is quad port ram
+parameter NUM_OF_PC       = 1;   // number of program counters
+parameter NUM_OF_INST_MEM = 1;   // number of instruction memory
 
-parameter NUM_OF_CONSTS = 32;  // number of constants is the cpuc
+parameter NUM_OF_CONSTS   = 8;  // number of constants is the cpuc
+
+//number of components that can be connected to amy register
+parameter NUM_OF_COMPONENTS  = NUM_OF_REGS + NUM_OF_MUL + NUM_OF_ADDERS + NUM_OF_CMP + NUM_OF_MUX + NUM_OF_EQUAL + 
+                               NUM_OF_SING_MEM + NUM_OF_DUAL_MEM + NUM_OF_QUAD_MEM + NUM_OF_PC;  
+
 parameter PROGRAM_SIZE  = 32;  // number of instructions
+
 
 typedef struct packed {
 
-    logic [NUM_OF_REGS-1:0][DATA_WIDTH-1:0] reg_outputs;
+    logic [NUM_OF_REGS+NUM_OF_PC-1:0][DATA_WIDTH-1:0] reg_outputs;
 
 } t_reg_outputs;
 
