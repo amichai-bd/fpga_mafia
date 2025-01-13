107d66d3a5b7a2b3e8d7e67c79f8765ab8bf70b1
roman gilgor <roman329@gmail.com>
2025-01-13 17:07:12 +0200

    add fibonacci design

---
+++ source/fibonacci/fibonacci_rtl_list.f
@@ -0,0 +1,11 @@
+
++incdir+../../../source/common/
+
+//package files
+../../../source/fibonacci/fibonacci_pkg.sv
+
+//RTL files
+../../../source/fibonacci/fibonacci.sv
+../../../source/fibonacci/fibonacci_st.sv
+
+
---
+++ verif/fibonacci/file_list/fibonacci_list.f
@@ -0,0 +1,5 @@
+
++define+SIM_ONLY
+-f ../../../source/fibonacci/fibonacci_rtl_list.f
+-f ../../../verif/fibonacci/file_list/fibonacci_verif_list.f
+
